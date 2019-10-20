df484f55938f7452bf56823286efe92451032c9b
a0972199950 <a0972199950@gmail.com>
2019-10-20 21:55:01 +0800

    修正bug

--- plugins/sweetAlert.ts
+++ plugins/sweetAlert.ts
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import swal from "sweetalert";
 
-declare module "vue/types/vue" {
-    interface Vue {
-        $swal: any
-    }
-}
+// declare module "vue/types/vue" {
+//     interface Vue {
+//         $swal: any
+//     }
+// }
 
 Vue.prototype.$swal = swal;
