21f6a8cba51f28aea473d1e9ed964d1aa0d85414
a0972199950 <a0972199950@gmail.com>
2019-10-23 20:51:14 +0800

    加入環境變數log檢查

--- server/db/mongoose.ts
+++ server/db/mongoose.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+console.log("db connect");
+console.log(process.env.MONGOOSE_DATABASE_URL);
+
 mongoose.connect(process.env.MONGOOSE_DATABASE_URL!, {
     useNewUrlParser: true,
     useCreateIndex: true,
