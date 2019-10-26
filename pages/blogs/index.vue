fe7d5d052a4371b6a97d005baf206afe14ef52a6
a0972199950 <a0972199950@gmail.com>
2019-10-26 20:39:36 +0800

    temp

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -29,6 +29,11 @@ export default class Blogs extends Vue {
     async asyncData(context: Context): Promise<IData | void> {
         const { app } = context;
 
+        console.log("asyncData MONGOOSE_DATABASE_URL: " + process.env.MONGOOSE_DATABASE_URL);
+        console.log("asyncData COOKIE_SIGN_KEY: " + process.env.COOKIE_SIGN_KEY);
+        console.log("asyncData JWT_SECRET_KEY: " + process.env.JWT_SECRET_KEY);
+        console.log("asyncData axios defaults: ", app.$axios.defaults);
+
         try {
             let { blogs }: { blogs: IBlogClient[] } = await app.$axios.$get("/api/blogs");
             return { blogs }
